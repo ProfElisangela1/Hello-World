eafda8629f9e1e99d42065e6c06c128491daf7f5
ProfElisangela1 <165838057+ProfElisangela1@users.noreply.github.com>
2024-04-03 20:42:57 -0300

    final

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let nomeUsuário = "";
-let elemento = document.querySelector("nome-usuário");
+let elemento = document.querySelector("#nome-usuário");
 
 while(nomeUsuário == ""){
-    nomeUsuário = prompt(Qual o seu nome?);
+    nomeUsuário = prompt("Qual o seu nome?");
 }
 
 if(nomeUsuário == null){
-    elemento.textContent = 'Seja muito bem-vindo.';
+    elemento.textContent = 'seja muito bem-vindo.';
 }else{
     elemento.textContent = nomeUsuário;
 }
